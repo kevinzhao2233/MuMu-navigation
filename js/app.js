c4f79a9e1fabeb34ec7557ae742e1a23cb4fa11b
kevin <2939575326@qq.com>
2019-09-05 11:04:36 +0800

    完成添加快捷按钮的任务

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 var searchENumber = 0;			// 默认为第一个搜索引擎
-
 var searchE = {
 	baidu: "https://m.baidu.com/s?word=",
 	google: "https://www.google.com/search?q=",
@@ -7,64 +6,10 @@ var searchE = {
 	shenma: "https://m.sm.cn/s?q=",
 	sougou: "https://www.sogou.com/web?query="
 }
-// 搜索
-function search(whSearch) {				// 接收 string
-	if ($('#searchInput').val() != "") {
-		window.location.href = searchE[whSearch + ""] + $('#searchInput').val();
-		$("#searchInput").val("");
-	}
-	return false;
-}
 
-// 改变搜索引擎
-function changeSearchE(whSearchE, offset) {
-	localStorage.setItem("SEARCH_E", whSearchE);
-	localStorage.setItem("SEARCH_E_ICO", offset);
-	$('#searchE_f_ico').css('transform', 'translateY(' + offset * -0.5 + 'rem)');
-	$('#searchE_b_ico').css('transform', 'translateY(' + offset * -0.5 + 'rem)');
-	$('#searchE_item span').removeClass('chose');
-	$('#searchE_item span').eq(searchENumber).addClass('chose');
-}
-
-//阻止默认行为函数
-function preventDefault(e) {
-	e.preventDefault();
-}
-
-// 阻止touchmove 之后调用 touchend    牺牲兼容性，优化性能
-function stopTouchendPropagationAfterScroll() {
-	var locked = false;
-	window.addEventListener('scroll', function () {
-		locked || (locked = true, window.addEventListener('touchend', stopTouchendPropagation, true));
-	}, true);
-	function stopTouchendPropagation(ev) {
-		ev.stopPropagation();
-		window.removeEventListener('touchend', stopTouchendPropagation, true);
-		locked = false;
-	}
-}
-stopTouchendPropagationAfterScroll();
 
 
 $(document).ready(function () {
-	// ======== 设置 html 的 fontsize，确定 rem 单位的大小
-	var screenWidth = $(window).width();
-	var screenHeight = $(window).height();
-	if (screenWidth < 720) {
-		$('html').css('font-size', screenWidth * 0.138889);
-	} else {
-		$('html').css('font-size', 100);
-	}
-	var temp = screenHeight / screenWidth;
-	// 给页面一个固定的vh
-	document.documentElement.style.setProperty("--origin_vh", screenHeight + "px");
-
-	// ======== 初始化搜索框图标 & 设置界面默认搜索引图标
-	searchENumber = localStorage.getItem("SEARCH_E_ICO");
-	$('#searchE_f_ico').css("transform", "translateY(" + searchENumber * -0.5 + "rem)");
-	$('#searchE_b_ico').css("transform", "translateY(" + searchENumber * -0.5 + "rem)");
-	$('#searchE_item span').removeClass('chose');
-	$('#searchE_item span').eq(searchENumber).addClass('chose');
 
 	// ======== 设置板块
 	$(document).on('touchend', '#setBtn', function () {
@@ -79,7 +24,7 @@ $(document).ready(function () {
 		document.removeEventListener('touchmove', preventDefault, { passive: false });
 	})
 
-	// 弹出搜索框 & 收回搜索框
+	// ======== 弹出搜索框 & 收回搜索框
 	$(document).on('touchend', '#searchFake', function () {
 		$('#arc').addClass('arc-active');
 		$('.up-search').addClass('up-search-active');
@@ -118,33 +63,18 @@ $(document).ready(function () {
 		$('#setBtn').addClass('set-btn-hidden');
 	})
 
+	// 退出编辑状态
+	$(document).on('touchend', '#editCancel', function(){
+		clearFastInput();
+	})
+
+	// 保存
 	$(document).on('touchend', '#editSave', function () {
-		$('#addFast').css('display', 'block');
 		addFastBtn();
 		if ($('#fastContent').children().length > 10) {
 			$('#addFast').css('display', 'none');
 		}
-		$('#fastEdit').removeClass('show-edit');
 		$('#setBtn').removeClass('set-btn-hidden');
 	})
 })
-
-function addFastBtn() {
-	if ($('#editUrl').val() != '' && $('#editTit').val() != '') {
-		var fastUrl = $('#editUrl').val();
-		var fastTit = $('#editTit').val();
-		var newElement = '<div class="fast-item bg2">' +
-			"<a href = " + fastUrl +
-			" class= 'fast-icon'>" +
-			'<img src="https://www.baidu.com/img/baidu_85beaf5496f291521eb75ba38eacbd87.svg" alt="">' +
-			'</a>' +
-			'<span class="fast-title">' + fastTit +
-			'</span>' +
-			'</div >'
-		$('#addFast').before(newElement);
-	}
-	else {
-		console.log("输入错误");
-	}
-}
 
