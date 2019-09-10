9d058389d1c202bcff833742fed944f8401d8980
kevin <2939575326@qq.com>
2019-09-10 16:08:59 +0800

    /* lottery */   完成纯CSS动画

--- pages/lottery/app.js
+++ pages/lottery/app.js
@@ -2,3 +2,20 @@
 // ======== 禁止页面滚动
 document.addEventListener('touchmove', preventDefault, { passive: false });
 
+
+$(document).ready(function(){
+
+})
+
+$(document).on('touchend', '#btn', function(){
+	setTimeout(function(){
+		$('#rotate').addClass('rotate-run');
+		$('#rotate').removeClass('rotate-start');
+	}, 1000);
+	setTimeout(function(){
+		$('#rotate').addClass('rotate-end');
+		$('#rotate').removeClass('rotate-run');
+	}, 4000);
+	$('#rotate').addClass('rotate-start');
+})
+
