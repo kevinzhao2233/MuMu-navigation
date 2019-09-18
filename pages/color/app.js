b17e97887970492ba4fcdb66b1728685c0618909
kevin <2939575326@qq.com>
2019-09-18 21:09:00 +0800

    增加功能
    增加 rgb颜色 转换 16进制 的功能

--- pages/color/app.js
+++ pages/color/app.js
@@ -12,18 +12,28 @@ $(document).on('touchend', function (ev) {
 			hex = $('#hexInput').val().toString().toUpperCase().replace('#', '');
 			// 合法检测
 			res = checkHex(hex);
-			if(res != 'err'){
+			if (res == 'err') {
+				$('#textColor').html('请输入正确的16进制颜色值！');
+				$('#textColor').addClass('err');
+				$('#show').addClass('show-color-err');
+			} else {
 				$('#textColor').html(res);
 				$('#show').css('background-color', res);
 				$('#show').addClass('show-color-act');
-			}else if(res === 'err'){
-				$('#textColor').html('请输入正确的16进制颜色值！');
-				$('#textColor').addClass('err');
 			}
 			break;
 		case 'rgbBtn':
-			rgb = $('#rgbInput').val().toUpperCase();
-			console.log("rgb", rgb);
+			rgb = $('#rgbInput').val().toString().toUpperCase().match(/\d+/g);
+			res = checkRgb(rgb);
+			if (res == 'err') {
+				$('#textColor').html('请输入正确的rgb()颜色值！');
+				$('#textColor').addClass('err');
+				$('#show').addClass('show-color-err');
+			} else {
+				$('#textColor').html(res);
+				$('#show').css('background-color', res);
+				$('#show').addClass('show-color-act');
+			}
 			break;
 	}
 })
@@ -40,18 +50,32 @@ function checkHex(hex) {
 			temp.push(hex[j * 2] + hex[j * 2 + 1])
 		}
 	} else if (hex.length === 8) {
-		// TODO: 对于 hex8 进行
+		// TODO: 对于 hex8 的支持
 	} else {
-		return('err');
+		return ('err');
 	}
-	for(var o = 0; o < 3; o++){
+	for (var o = 0; o < 3; o++) {
 		result[o] = parseInt(temp[o], 16);
 	}
 	console.log(result.toString());
-	return('rgb(' + result.toString() + ')');
+	return ('rgb(' + result.toString() + ')');
 }
 
 function checkRgb(rgb) {
-	var temp = [];
+	var temp = rgb.concat();
 	var result = [];
+	for (var i = 0; i < 3; i++) {
+		if (temp[i] > 255) {
+			return ('err')
+		} else {
+			result[i] = parseInt(temp[i]).toString(16);		// 进制转换是数字之间的转换
+		}
+	}
+	// console.log();
+	for (var j = 0; j < 3; j++) {
+		if (result[j].toString().length == 1) {
+			result[j] = '0' + result[j];
+		}
+	}
+	return ('#' + result[0] + result[1] + result[2]);
 }
